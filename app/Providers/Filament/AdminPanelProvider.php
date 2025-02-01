29362d134e56adf660ae79d175a9fa6eb5f3abb7
Taylor Cox <taylor@posidev.com>
2025-02-01 00:44:37 -0500

    Top nav

--- app/Providers/Filament/AdminPanelProvider.php
+++ app/Providers/Filament/AdminPanelProvider.php
@@ -28,8 +28,11 @@ class AdminPanelProvider extends PanelProvider
             ->path('admin')
             ->login()
             ->colors([
-                'primary' => Color::Indigo,
+                'primary' => Color::Sky,
             ])
+            ->spa()
+            ->topNavigation()
+            ->breadcrumbs(false)
             ->viteTheme('resources/css/filament/admin/theme.css')
             ->discoverResources(in: app_path('Filament/Resources'), for: 'App\\Filament\\Resources')
             ->discoverPages(in: app_path('Filament/Pages'), for: 'App\\Filament\\Pages')
